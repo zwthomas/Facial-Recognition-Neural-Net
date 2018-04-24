bb488b6c6d291f5776ae21f6feccaba10593001d
Zach <zwthomas13@gmail.com>
2018-04-24 10:41:54 -0500

    First

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string fileName = "an2i_left_happy_sunglasses_4.pgm";
+    ifstream fileInputs;                    // Open the file
+    fileInputs.open(fileName, ios::in);
+    string in;
+    fileInputs >> in;           // Type
+    int width, height, max, pixel;
+    fileInputs >> width;
+    fileInputs >> height;
+    fileInputs >> max;
+
+    cout << width << " " << height << " " << max << endl;
+
     return 0;
 }
