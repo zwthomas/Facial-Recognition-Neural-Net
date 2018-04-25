481db41f90bff6e43b6093c670fa5d3c085ba24c
zwthomas <zwthomas13@gmail.com>
2018-04-25 09:19:47 -0500

    First

--- main.cpp
+++ main.cpp
@@ -2,18 +2,30 @@
 #include <fstream>
 using namespace std;
 
+double* parseImage(string filename);
+
+extern int IMAGE_WIDTH;
+extern int IMAGE_HEIGHT;
+
 int main() {
-    string fileName = "an2i_left_happy_sunglasses_4.pgm";
+    string filename = "an2i_left_happy_sunglasses_4.pgm";
+    parseImage(filename);
+
+    return 0;
+}
+
+double* parseImage(string filename) {
     ifstream fileInputs;                    // Open the file
-    fileInputs.open(fileName, ios::in);
+    fileInputs.open(filename, ios::in);
+
     string in;
     fileInputs >> in;           // Type
     int width, height, max, pixel;
-    fileInputs >> width;
-    fileInputs >> height;
+    fileInputs >> IMAGE_WIDTH;
+    fileInputs >> IMAGE_HEIGHT;
     fileInputs >> max;
 
-    cout << width << " " << height << " " << max << endl;
+    cout << IMAGE_WIDTH << " " << IMAGE_HEIGHT << " " << max << endl;
 
-    return 0;
+
 }
